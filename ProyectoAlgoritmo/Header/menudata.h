17c8d5dcc190c90a963661c97877511602fe9c14
HYAR7E <psiro.hyarte@gmail.com>
2019-11-12 13:13:42 -0500

    v0.6 Fixed bug in printJobOffers(), now it works fine for everybody

--- ProyectoAlgoritmo/Header/menudata.h
+++
@@ -1,9 +0,0 @@
-// Menu data storage
-#ifndef MENU_DATA
-#define MENU_DATA
-// String variables
-struct Menu{
-    const string _menu = "Find my worker App";
-    const string _menuElements = "Welcome to our job matching app";
-};
-#endif
